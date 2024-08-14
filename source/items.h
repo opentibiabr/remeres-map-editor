cc04473c34ec41e0eed9231d9ee29399439f9bef
Pedro Henrique Alves Cruz <phac@cin.ufpe.br>
2024-08-14 01:53:35 -0300

    feat: show item avoidables (block pathfinder)

--- source/items.h
+++ source/items.h
@@ -383,6 +383,10 @@ public:
 	bool blockPathfinder;
 	bool hasElevation;
 
+	bool isAnimation;
+	bool fullGround;
+	bool forceUse;
+
 	int alwaysOnTopOrder;
 	uint16_t rotateTo;
 	BorderType border_alignment;
